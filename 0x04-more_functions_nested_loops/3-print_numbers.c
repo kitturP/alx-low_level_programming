5a6b1c11a8698a05b809b35f0ee315e89ab3cb72
kitturP <48672176+kitturP@users.noreply.github.com>
2022-04-14 15:39:05 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,14 +10,14 @@
  */
 void print_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar('0' + i);
-		if (i == 9)
-		{
-			_putchar('\n');
-		}
-	}
+for (i = 0; i <= 9; i++)
+{
+_putchar('0' + i);
+if (i == 9)
+{
+_putchar('\n');
+}
+}
 }
