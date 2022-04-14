81c7918d5038cb396b27e9af2fe3c535a89bb5c0
kitturP <48672176+kitturP@users.noreply.github.com>
2022-04-14 16:31:36 -0700

    Write a function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,32 +1,22 @@
 #include "main.h"
+
 /**
-* print_number - that prints an integer
-*@n: print int
-* Return: Always 0.
-*/
+ * print_number - prints an integer
+ * @n: tracked var
+ */
+
 void print_number(int n)
 {
-	unsigned int x, y, count;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		x = -n;
+		x = -x;
 	}
-	else
+	if ((x / 10) > 0)
 	{
-		x = n;
+		print_number(x / 10);
 	}
-	y = x;
-	count = 1;
-
-	while (y > 9)
-	{
-		y /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((x / count) % 10) + 48);
-	}
+	_putchar((x % 10) + 48);
 }
