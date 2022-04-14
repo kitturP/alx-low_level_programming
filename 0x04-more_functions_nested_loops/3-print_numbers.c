39f439360b1af3a5b35a6cad556f7ae582b28920
kitturP <48672176+kitturP@users.noreply.github.com>
2022-04-14 07:34:04 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,19 @@
 
 /**
  * print_numbers - print numbers
+ *
  * Return: Always 0.
  */
+
 void print_numbers(void)
 {
 int i;
 
 i = 0;
-while (i < 10)
-{
+
+for (i = 0;i < 10; i++)
+
 _putchar (i + '0');
-i++;
-}
 
 _putchar ('\n');
 }
